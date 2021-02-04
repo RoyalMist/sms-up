630fb03674496c8ba01432b123649c3f922d9d4e
PrisM <thibault.fouache@gmail.com>
2021-02-04 16:45:59 +0100

    1.0.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SmsUp.MixProject do
     [
       app: :sms_up,
       elixir: "~> 1.11",
-      version: "0.5.0",
+      version: "1.0.0",
       source_url: "https://github.com/RoyalMist/sms_up",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
