da96c93ab4bc35ad9cffa2fc853909bb865f6622
Thibault Fouache <thibault.fouache@gmail.com>
2023-08-07 21:11:31 +0200

    Format

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,11 +1,11 @@
 defmodule TestCluster do
   def start_slaves(number) do
     :ok = :net_kernel.monitor_nodes(true)
-    _ = :os.cmd('epmd -daemon')
+    _ = :os.cmd(~c"epmd -daemon")
     Node.start(:master@localhost, :shortnames)
 
     Enum.each(1..number, fn index ->
-      :slave.start_link(:localhost, 'slave_#{index}')
+      :slave.start_link(:localhost, ~c"slave_#{index}")
     end)
 
     [node() | Node.list()]
